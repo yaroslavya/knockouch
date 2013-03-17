413728ec834d9103088855a5f991ce70e2a4c20e
Yaroslav Yakovlev <yaroslavya@gmail.com>
2013-03-18 04:24:17 +0800

    initial knockouch version.

--- knockouch/src/knockouch.js
+++ knockouch/src/knockouch.js
@@ -1,14 +1,18 @@
 ﻿
 (function (ko, touch) {
-    //TODO: wrap everything in a loop for every gesture. After that we will be able to add all gestures at once.
-    ko.bindingHandlers.tap = {
-        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            //TODO:also make sure we are providing the event data to the handler to react smartly.
-            var handler = valueAccessor();
-            Hammer(element).on('tap', handler);
-        },
-        update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-            //TODO: we can make some tricky way to unregister any handlers here. 
-        }
+    function makeTouchHandlerShortcut(touchEventName) {        
+        ko.bindingHandlers[touchEventName] = {
+            init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+                var handler = valueAccessor();
+                Hammer(element).on(touchEventName, handler);
+            }
+        };
     };
+    
+    var touchEvents = ['tap', 'doubletap', 'hold', 'swipe', 'drag', 'transform'];
+    for (var i = 0; i < touchEvents.length; i++) {
+        var eventName = touchEvents[i];
+        makeTouchHandlerShortcut(eventName);
+    }
+
 }(ko, Hammer));
