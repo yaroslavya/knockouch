c2ea902b1e337f38856697c79c06d0b6a9e71c53
Mainmatsu <Mainmatsu@gmail.com>
2013-03-20 02:41:45 +0800

    optimize cycle for

--- knockouch/src/knockouch.js
+++ knockouch/src/knockouch.js
@@ -1,23 +1,23 @@
-﻿
-(function (ko, touch) {
-    function makeTouchHandlerShortcut(touchEventName) {        
+﻿(function (ko, touch) {
+    function makeTouchHandlerShortcut(touchEventName) {
         ko.bindingHandlers[touchEventName] = {
             init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
                 var handler = valueAccessor();
+                var allBindings = allBindingsAccessor();
                 Hammer(element).on(touchEventName, handler);
             }
         };
     };
-    
-    var touchEvents = ['tap', 'doubletap', 'hold', 'rotate', 
-                       'drag', 'dragleft', 'dragright', 'dragup', 'dragdown', 
-                       'transform', 'transformstart', 'transformend', 
-                       'swipe', 'swipeleft', 'swiperight', 'swipeup', 'swipedown', 
-                       'pinch', 'pinchin', 'pinchout'];
-    
-    for (var i = 0; i < touchEvents.length; i++) {
+
+    var touchEvents = ['tap', 'doubletap', 'hold', 'rotate',
+                       'drag', 'dragleft', 'dragright', 'dragup',
+                       'dragdown', 'transform', 'transformstart',
+                       'transformend', 'swipe', 'swipeleft', 'swiperight',
+                       'swipeup', 'swipedown','pinch', 'pinchin', 'pinchout'];
+
+
+    for (i in touchEvents) {
         var eventName = touchEvents[i];
         makeTouchHandlerShortcut(eventName);
     }
-
 }(ko, Hammer));
