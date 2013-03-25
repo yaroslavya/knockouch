95ad6cf04a78c850dfb04b10ff23b2e74e5e6054
Saymon <mainmatsu@gmail.com>
2013-03-25 17:07:41 +0800

    add support for non hammer type events

--- knockouch/src/knockouch.js
+++ knockouch/src/knockouch.js
@@ -4,9 +4,17 @@
         selectTouchLibrary(lib);
     };
 
+<<<<<<< HEAD
     knockouch.touchLibrary = {};
     knockouch.touch = {};
     knockouch.eventList = [];
+=======
+    knockouch.touchlibrarys = ['Hammer', 'Zepto'];
+    knockouch.touchLibrary = {};
+    knockouch.touch = {};
+    knockouch.touchEvents = [];
+    
+>>>>>>> add support for non hammer type events
 
     function makeTouchHandlerShortcut(touchEventName) {
         ko.bindingHandlers[touchEventName] = {
@@ -35,8 +43,13 @@
 
     function setMoreOptions(bindings) {
         var options = knockouch.options;
+<<<<<<< HEAD
         for (i in knockouch.HammerOptions) {
             var optionName = knockouch.HammerOptions[i];
+=======
+        for (i in knockouch.hammerOptions) {
+            var optionName = knockouch.hammerOptions[i];
+>>>>>>> add support for non hammer type events
             if (bindings[optionName] !== undefined && bindings[optionName].constructor !== Function) {
                 knockouch.options[optionName] = bindings[optionName];
             }
@@ -44,27 +57,71 @@
         return options;
     };
 
+<<<<<<< HEAD
+=======
+    function init() {
+        var library = knockouch.touchLibrary;
+        knockouch.touch = knockouch[library + "Wrapper"];
+        for (i in knockouch.touchEvents) {
+            var eventName = knockouch.touchEvents[i];
+            makeTouchHandlerShortcut(eventName);
+        }
+    };
+
+    function unifyEventName(eventName) {
+        if (knockouch.ZeptoReplacementEvents[eventName] !== undefined) {
+            return knockouch.ZeptoReplacementEvents[eventName];
+        }
+        else {
+            throw "you've selected touch library not support " + eventName + " event";
+        }
+    };
+
+>>>>>>> add support for non hammer type events
     knockouch.HammerWrapper = function (element, touchEventName, handler, bindings) {
         var options = setMoreOptions(bindings);
         Hammer(element, options).on(touchEventName, handler);
     };
 
     knockouch.ZeptoWrapper = function (element, touchEventName, handler) {
+<<<<<<< HEAD
         Zepto(element)[touchEventName](handler);
     };
 
     knockouch.touchlibrarys = ['Hammer', 'Zepto'];
 
     knockouch.HammerTouchEvents  = ['tap', 'doubletap', 'hold', 'rotate',
+=======
+        touchEventName = unifyEventName(touchEventName);
+        Zepto(element)[touchEventName](handler);
+    };
+
+    knockouch.touchEvents  = ['tap', 'doubletap', 'hold', 'rotate',
+>>>>>>> add support for non hammer type events
                        'drag', 'dragleft', 'dragright', 'dragup',
                        'dragdown', 'transform', 'transformstart',
                        'transformend', 'swipe', 'swipeleft', 'swiperight',
                        'swipeup', 'swipedown', 'pinch', 'pinchin', 'pinchout'];
 
+<<<<<<< HEAD
     knockouch.ZeptoTouchEvents = ['swipe', 'swipeLeft', 'swipeRight', 'swipeUp',
                             'swipeDown', 'doubleTap', 'tap', 'singleTap', 'longTap']
 
     knockouch.HammerOptions = ['doubletap_distance', 'doubletap_interval', 'drag',
+=======
+    knockouch.ZeptoReplacementEvents = {
+        'swipeleft': 'swipeLeft',
+        'swiperight': 'swipeRight',
+        'swipeup': 'swipeUp',
+        'swipedown': 'swipeDown',
+        'doubletap': 'doubleTap',
+        'hold': 'longTap',
+        'tap': 'tap',
+        'swipe': 'swipe'
+    };
+
+    knockouch.hammerOptions = ['doubletap_distance', 'doubletap_interval', 'drag',
+>>>>>>> add support for non hammer type events
                         'drag_block_horizontal', 'drag_block_vertical', 'drag_lock_to_axis',
                         'drag_max_touches', 'drag_min_distance', 'hold',
                         'hold_threshold', 'hold_timeout', 'prevent_default',
@@ -74,6 +131,7 @@
                         'touch', 'transform', 'transform_always_block',
                         'transform_min_rotation', 'transform_min_scale'];
 
+<<<<<<< HEAD
     function init() {
         var library = knockouch.touchLibrary;
         knockouch.eventList = knockouch[library + "TouchEvents"];
@@ -83,6 +141,9 @@
             makeTouchHandlerShortcut(eventName);
         }
     };
+=======
+    
+>>>>>>> add support for non hammer type events
 
     window.knockouch = knockouch;
     searchTouchLibrary();
