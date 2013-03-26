3558058418d5ea572c3e89407c5f3a04c549f20b
Saymon <mainmatsu@gmail.com>
2013-03-26 20:09:39 +0800

    Global changes for optimize it

--- knockouch/src/knockouch.js
+++ knockouch/src/knockouch.js
@@ -1,67 +1,70 @@
 ﻿(function (windows, ko) {
-    var knockouch = function (lib, options) {
+    var knockouch = function (library, options) {
         knockouch.options = options || {};
-        knockouch.selectTouchLibrary(lib);
+        knockouch.selectTouchLibrary(library);
     };
 
-    knockouch.touchlibrarys = ['Hammer', 'jQuery', 'Zepto'];
-    knockouch.touchLibrary = "";
-    knockouch.replacementEvents = {};
-    knockouch.touch = {};
-    
+    knockouch.touchLibrary = {};
+    knockouch.touchLibraries = {};
+    knockouch.touchEvents = ['tap', 'doubletap', 'hold', 'rotate',
+                       'drag', 'dragleft', 'dragright', 'dragup',
+                       'dragdown', 'transform', 'transformstart',
+                       'transformend', 'swipe', 'swipeleft', 'swiperight',
+                       'swipeup', 'swipedown', 'pinch', 'pinchin', 'pinchout'];
     
     knockouch.makeTouchHandlerShortcut = function(touchEventName) {
         ko.bindingHandlers[touchEventName] = {
             init: function (element, valueAccessor, allBindingsAccessor) {
                 var handler = valueAccessor();
                 var allBindings = allBindingsAccessor();
-                knockouch.touch(element, touchEventName, handler, allBindings);
+                knockouch.touchLibrary.wrapper(element, touchEventName, handler, allBindings);
             }
         };
     };
 
     knockouch.searchTouchLibrary = function() {
-        for (i in knockouch.touchlibrarys) {
-            var touchLibary = knockouch.touchlibrarys[i];
-            if (window[touchLibary] !== undefined) {
-                knockouch.selectTouchLibrary(touchLibary);
+        for (i in knockouch.touchLibraries) {
+            var touchLibary = knockouch.touchLibraries[i];
+            if (touchLibary.path !== undefined) {
+                knockouch.touchLibrary = touchLibary;
                 break;
             }
         }
     };
 
-    knockouch.selectTouchLibrary = function(library) {
-        knockouch.touchLibrary = library;
-        knockouch.init();
-    };
-
-    knockouch.init = function() {
-        var library = knockouch.touchLibrary;
-        knockouch.touch = knockouch[library + 'Wrapper'];
-        knockouch.replacementEvents = knockouch[library + 'ReplacementEvents'];
-        for (i in knockouch.touchEvents) {
-            var eventName = knockouch.touchEvents[i];
-            knockouch.makeTouchHandlerShortcut(eventName);
+    knockouch.selectTouchLibrary = function (library) {
+        if (knockouch.touchLibraries[library] !== undefined) {
+            knockouch.touchLibrary = knockouch.touchLibraries[library];
+        }
+        else {
+            throw 'failed to select a library ' + library +
+            ' perhaps you misspelled her name or library ' + library +
+            ' not support. Please refer to our documentation.';
         }
     };
 
-    knockouch.unifyEventName = function(eventName) {
-        if (knockouch.replacementEvents[eventName] !== undefined) {
-            return knockouch.replacementEvents[eventName];
+    knockouch.unifyEventName = function (eventName, eventsRequiringReplacement) {
+        if (eventsRequiringReplacement[eventName] !== undefined) {
+            return eventsRequiringReplacement[eventName];
         }
         else {
             throw "you've selected touch library not support " + eventName + ' event';
         }
     };
 
-    knockouch.touchEvents  = ['tap', 'doubletap', 'hold', 'rotate',
-                       'drag', 'dragleft', 'dragright', 'dragup',
-                       'dragdown', 'transform', 'transformstart',
-                       'transformend', 'swipe', 'swipeleft', 'swiperight',
-                       'swipeup', 'swipedown', 'pinch', 'pinchin', 'pinchout'];
+    knockouch.init = function() {
+        for (i in knockouch.touchEvents) {
+            var eventName = knockouch.touchEvents[i];
+            knockouch.makeTouchHandlerShortcut(eventName);
+        }
+        knockouch.searchTouchLibrary();
+    };
 
-    //--------Hammer--------------
-    knockouch.hammerOptions = ['doubletap_distance', 'doubletap_interval', 'drag',
+    knockouch.touchLibraries.Hammer = {
+        name: "Hammer",
+        path: window.Hammer,
+        options :{},
+        optionsList : ['doubletap_distance', 'doubletap_interval', 'drag',
                         'drag_block_horizontal', 'drag_block_vertical', 'drag_lock_to_axis',
                         'drag_max_touches', 'drag_min_distance', 'hold',
                         'hold_threshold', 'hold_timeout', 'prevent_default',
@@ -69,59 +72,59 @@
                         'swipe', 'swipe_max_touches', 'swipe_velocity',
                         'tap', 'tap_max_distance', 'tap_max_touchtime',
                         'touch', 'transform', 'transform_always_block',
-                        'transform_min_rotation', 'transform_min_scale'];
-
-    knockouch.setMoreOptions = function(bindings) {
-        var options = knockouch.options;
-        for (i in knockouch.hammerOptions) {
-            var optionName = knockouch.hammerOptions[i];
-            if (bindings[optionName] !== undefined && bindings[optionName].constructor !== Function) {
-                knockouch.options[optionName] = bindings[optionName];
+                        'transform_min_rotation', 'transform_min_scale'],
+        setMoreOptions: function(bindings) {
+            var extendedOptions = this.options;
+            for (i in this.optionsList) {
+                var optionName = this.optionsList[i];
+                if (bindings[optionName] !== undefined && bindings[optionName].constructor !== Function) {
+                    this.options[optionName] = bindings[optionName];
+                }
             }
+            return extendedOptions;
+        },
+        wrapper: function (element, touchEventName, handler, bindings) {
+            var extendedOptions = this.setMoreOptions(bindings);
+            Hammer(element, extendedOptions).on(touchEventName, handler);
         }
-        return options;
     };
 
-    knockouch.HammerWrapper = function (element, touchEventName, handler, bindings) {
-        var options = knockouch.setMoreOptions(bindings);
-        Hammer(element, options).on(touchEventName, handler);
+    knockouch.touchLibraries.jQueryMobile = {
+        name: "jQuery Mobile",
+        path: window.jQuery.mobile,
+        eventsRequiringReplacement: {
+            'swipeleft': 'swipeLeft',
+            'swiperight': 'swipeRight',
+            'hold': 'taphold',
+            'tap': 'tap',
+            'swipe': 'swipe'
+        },
+        wrapper: function (element, touchEventName, handler) {
+            touchEventName = knockouch.unifyEventName(touchEventName, this.eventsRequiringReplacement);
+            jQuery(element).bind(touchEventName, handler);
+        }
     };
 
-    //----------------------------
-
-    //--------jQuery--------------
-    knockouch.jQueryReplacementEvents = {
-        'swipeleft': 'swipeLeft',
-        'swiperight': 'swipeRight',
-        'hold': 'taphold',
-        'tap': 'tap',
-        'swipe': 'swipe'
+    knockouch.touchLibraries.Zepto = {
+        name: "Zepto",
+        path: window.Zepto,
+        eventsRequiringReplacement: {
+            'swipeleft': 'swipeLeft',
+            'swiperight': 'swipeRight',
+            'swipeup': 'swipeUp',
+            'swipedown': 'swipeDown',
+            'doubletap': 'doubleTap',
+            'hold': 'longTap',
+            'tap': 'tap',
+            'swipe': 'swipe'
+        },
+        wrapper: function (element, touchEventName, handler) {
+            touchEventName = knockouch.unifyEventName(touchEventName,this.eventsRequiringReplacement);
+            Zepto(element)[touchEventName](handler);
+        }
     };
 
-    knockouch.jQueryWrapper = function (element, touchEventName, handler, bindings) {
-        jQuery(element).bind(touchEventName, handler);
-    };
-    //----------------------------
-
-    //--------Zepto---------------
-    knockouch.ZeptoReplacementEvents = {
-        'swipeleft': 'swipeLeft',
-        'swiperight': 'swipeRight',
-        'swipeup': 'swipeUp',
-        'swipedown': 'swipeDown',
-        'doubletap': 'doubleTap',
-        'hold': 'longTap',
-        'tap': 'tap',
-        'swipe': 'swipe'
-    };
-
-    knockouch.ZeptoWrapper = function (element, touchEventName, handler) {
-        touchEventName = knockouch.unifyEventName(touchEventName);
-        Zepto(element)[touchEventName](handler);
-    };
-
-    //----------------------------
+    knockouch.init();
     window.knockouch = knockouch;
-    knockouch.searchTouchLibrary();
 
 }(this, ko));
